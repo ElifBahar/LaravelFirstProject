bd9388b2fc31183abeb50bd5c4ef3e5588dbd23f
ElifBahar <elif.har94@gmail.com>
2022-02-20 13:12:15 +0300

    add saving categories backend functions, add session successs message for category frontend

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CategoryController extends Controller
 {
@@ -18,5 +21,19 @@ class CategoryController extends Controller
                 'category_name.required' => 'Please input category name'
             ]
         );
+
+        Category::insert([
+           'category_name' => $request->category_name,
+           'user_id' => Auth::user()->id,
+            'created_at' => Carbon::now()
+        ]);
+
+        return Redirect()->back()->with('success','Category Inserted Successfully');
+
+        /*$category = new Category;
+        $category->category_name = $request->category_name;
+        $category->user_id = Auth::user()->id;
+        $category->save();
+        */
     }
 }
--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -11,6 +11,12 @@
             <div class="row">
                 <div class="col-md-8">
                     <div class="card">
+                        @if(session('success'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <strong>{{ session('success') }}</strong>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        @endif
                         <div class="card-header">All Category</div>
                         <table class="table">
                             <thead>
