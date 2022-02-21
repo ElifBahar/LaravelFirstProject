a405eefb39b945c00fb4609d1a69ab0bf52eb31a
ElifBahar <elif.har94@gmail.com>
2022-02-21 13:29:19 +0300

    add resize image insert withimage intervention package to store brand controller

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class BrandController extends Controller
 {
@@ -24,12 +25,18 @@ class BrandController extends Controller
         ]);
 
         $brand_image = $request->file('brand_image');
+        /*
         $name_gen = hexdec(uniqid());
         $img_ext = strtolower($brand_image->getClientOriginalExtension());
         $img_name = $name_gen.'.'.$img_ext;
         $up_location = 'image/brand/';
         $last_img = $up_location.$img_name;
-        $brand_image->move($up_location,$img_name);
+        $brand_image->move($up_location,$img_name);*/
+
+        $name_gen = hexdec(uniqid()).'.'.$brand_image->getClientOriginalExtension();
+        Image::make($brand_image)->resize(300,200)->save('image/brand/'.$name_gen);
+
+        $last_img = 'image/brand/'.$name_gen;
 
         Brand::insert([
             'brand_name' => $request->brand_name,
