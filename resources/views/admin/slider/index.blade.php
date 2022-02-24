79d5002d60ef36a9d2806f3e28917cd5d022a5ef
ElifBahar <elif.har94@gmail.com>
2022-02-25 01:13:20 +0300

    add slider items dynamically to the front home page

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -23,11 +23,11 @@
                         <table class="table">
                             <thead>
                             <tr>
-                                <th scope="col">SL No</th>
-                                <th scope="col">Slider Title</th>
-                                <th scope="col">Description</th>
-                                <th scope="col">Image</th>
-                                <th scope="col">Action</th>
+                                <th scope="col" width="5%">SL No</th>
+                                <th scope="col" width="15%">Slider Title</th>
+                                <th scope="col" width="25%">Description</th>
+                                <th scope="col" width="15%">Image</th>
+                                <th scope="col" width="15%">Action</th>
                             </tr>
                             </thead>
                             <tbody>
