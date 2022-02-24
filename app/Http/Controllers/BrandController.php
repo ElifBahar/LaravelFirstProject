3aa954891185eba6c24cce9308a907b0bcedd016
ElifBahar <elif.har94@gmail.com>
2022-02-24 19:29:12 +0300

    add middleware auth control to brand page

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -10,6 +10,10 @@ use Intervention\Image\Facades\Image;
 
 class BrandController extends Controller
 {
+
+    public function __construct(){
+        $this->middleware('auth');
+    }
     public function AllBrand(){
         $brands = Brand::latest()->paginate(5);
         return view('admin.brand.index',compact('brands'));
