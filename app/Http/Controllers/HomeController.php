db2e283ecbb7899d9b72e338eb8c587386657682
ElifBahar <elif.har94@gmail.com>
2022-02-25 01:24:56 +0300

    add edit and update functşons to slider items

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,4 +34,56 @@ class HomeController extends Controller
 
         return Redirect()->route('home.slider')->with('success','Slider Inserted Successfully');
     }
+
+    public function SliderEdit($id){
+        $sliders = Slider::find($id);
+        return view('admin.slider.edit',compact('sliders'));
+    }
+
+    public function SliderUpdate(Request $request, $id){
+        $validateData =  $request->validate([
+            'title' => 'required|min:4',
+            'description' => 'required'
+        ],
+            [
+                'title.required' => 'Please Input Brand Name',
+                'title.min' => 'Brand name must be longer than 4 characters'
+            ]);
+
+        $old_image = $request->old_image;
+
+
+        $slider_image = $request->file('image');
+
+        if($slider_image){
+            $name_gen = hexdec(uniqid());
+            $img_ext = strtolower($slider_image->getClientOriginalExtension());
+            $img_name = $name_gen.'.'.$img_ext;
+            $up_location = 'image/slider/';
+            $last_img = $up_location.$img_name;
+            $slider_image->move($up_location,$img_name);
+
+            unlink($old_image);
+            Slider::find($id)->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'image' => $last_img,
+                'created_at' => Carbon::now()
+            ]);
+
+            return Redirect()->back()->with('success','Slider Item Updated Successfully');
+
+        }else{
+            Slider::find($id)->update([
+                'title' => $request->title,
+                'description' => $request->description,
+                'created_at' => Carbon::now()
+            ]);
+
+            return Redirect()->back()->with('success','Slider Updated Successfully');
+
+        }
+
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,9 @@ Route::post('/multi/add',[BrandController::class, 'StoreImg'])->name('store.imag
 Route::get('/home/slider',[HomeController::class, 'HomeSlider'])->name('home.slider');
 Route::get('/add/slider',[HomeController::class, 'AddSlider'])->name('add.slider');
 Route::post('/store/slider',[HomeController::class, 'StoreSlider'])->name('store.slider');
+Route::get('/slider/edit/{id}',[HomeController::class, 'SliderEdit']);
+Route::post('/slider/update/{id}',[HomeController::class, 'SliderUpdate']);
+Route::get('/slider/delete/{id}',[HomeController::class, 'SliderDelete']);
 
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
